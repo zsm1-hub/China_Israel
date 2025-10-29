0b57203ac01a7e29b56f6707e38bef4647afec9d
Siming Zhang <104129054+zsm1-hub@users.noreply.github.com>
2025-10-29 20:29:44 +0800

    Add files via upload

--- check_smallrough_Lag_Eul_all.m
+++ check_smallrough_Lag_Eul_all.m
@@ -12,7 +12,9 @@ win='kaiser'
 ini='_roughsmall'
 str1=15;
 end1=26;
-inv='NNLS'
+% str1=1;
+% end1=19;
+inv='RLS'
 yy1=-0.6e-8;
 yy2=-1.0e-8;
 yy3=-1.4e-8;
@@ -20,11 +22,30 @@ yy4=-1.8e-8;
 timerange=1:2148;
 param_Eul_cg_sf3fk
 
-% % wave, _roughsmall
-lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
+
+if strcmpi(Case, 'wave')
+    Casemean='Hf';
+    lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
     1e-7,1e-8,1e-9,1e-10,1e-11,2e-11,1e-12];
-range1=2.5e3;
-range2=200e3;
+    % lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
+    % 1e-7,1e-8,1e-9,1e-10,1e-11,2e-11,1e-12,1e-13,1e-14];
+    range1=2.5e3;
+    range2=200e3;
+
+end
+if strcmpi(Case, 'nowave')
+    Casemean='Sm';
+    lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
+    1e-7,1e-8,1e-9,1e-10,1e-11,2e-11,1e-12,1e-13,1e-14,1e-15,1e-16];
+    range1=2.5e3;
+    range2=140e3;
+end
+
+% % wave, _roughsmall
+% lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
+%     1e-7,1e-8,1e-9,1e-10,1e-11,2e-11,1e-12];
+% range1=2.5e3;
+% range2=200e3;
 
 % nowave
 % lambda=[10,1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6, ...
@@ -89,6 +110,7 @@ SF3_Lag2500=nanmean(SF3_2500,2);
 for i = 1:size(SF3_Lag2500,1)
     CI_SF3_Lag2500(:,i) = prctile(SF3_2500(i,:), [95, 5]); 
 end
+
 dd1=dist_axis(1:end-1);
 dd1=dd1(str1:end1);
 x_fill2 = [dd1./1e3, fliplr(dd1)./1e3];
@@ -97,18 +119,28 @@ cisf3_289=CI_SF3_Lag289(:,1:end-1);
 cisf3_289=cisf3_289(:,str1:end1);
 y_fillSF3_289=[cisf3_289(1,:),...
     fliplr(cisf3_289(2,:))]./x_fill2./1e3;
+y_fillebs_289=[CI_ebs289(1,:),...
+    fliplr(CI_ebs289(2,:))];
+
 
 
 cisf3_625=CI_SF3_Lag625(:,1:end-1);
 cisf3_625=cisf3_625(:,str1:end1);
 y_fillSF3_625=[cisf3_625(1,:),...
     fliplr(cisf3_625(2,:))]./x_fill2./1e3;
+y_fillebs_625=[CI_ebs625(1,:),...
+    fliplr(CI_ebs625(2,:))];
+
 
 cisf3_2500=CI_SF3_Lag2500(:,1:end-1);
 cisf3_2500=cisf3_2500(:,str1:end1);
 y_fillSF3_2500=[cisf3_2500(1,:),...
     fliplr(cisf3_2500(2,:))]./x_fill2./1e3;
+y_fillebs_2500=[CI_ebs2500(1,:),...
+    fliplr(CI_ebs2500(2,:))];
+% 
 
+dk_L=u2rho_2d(abs(diff(kf_Lag)));
 x_fill = [1./kf_Lag(1:end)./1e3.*2.*pi, fliplr(1./kf_Lag(1:end)./1e3.*2.*pi)];
 
 x_coords = [4, 200, 200, 4];  % x坐标：左边界4，右边界200
@@ -158,7 +190,7 @@ ylabel('$$\mathbf{F(r) \ [m^{2}/s^{3}]}$$','Interpreter','latex')
 % legend([b1,b2],{['CG (',win,')'],'SF3-fitting (RLS)'})
 legend([b1,b2,B1,B2],{['Eul CG (',win,')'],['Eul ',inv,'-fitting'], ...
     ['Lag CG (',win,')'],['Lag ',inv,'-fitting']})
-text(0.03, 0.95, 'a) Hf P289', 'Units', 'normalized', ...
+text(0.03, 0.95, ['c) ',Casemean,' P289'], 'Units', 'normalized', ...
      'FontSize', 12, 'FontWeight', 'bold', ...
      'BackgroundColor', [1, 1, 0.8, 0.6], ... % 半透明背景
      'EdgeColor', [.7,.7,.7], ... % 边框颜色
@@ -194,7 +226,7 @@ xlabel('$$\mathbf{r \ [km]}$$','Interpreter','latex')
 ylabel('$$\mathbf{D3(r)/r \ [m^{2}/s^{3}]}$$','Interpreter','latex')
 
 legend([b1,b2],{['Lag D3(r)/r'],'fitting'},'Location','southeast')
-text(0.03, 0.95, 'd) Hf P289', 'Units', 'normalized', ...
+text(0.03, 0.95, ['c) ',Casemean,' P289'], 'Units', 'normalized', ...
      'FontSize', 12, 'FontWeight', 'bold', ...
      'BackgroundColor', [1, 1, 0.8, 0.6], ... % 半透明背景
      'EdgeColor', [.7,.7,.7], ... % 边框颜色
@@ -202,7 +234,33 @@ text(0.03, 0.95, 'd) Hf P289', 'Units', 'normalized', ...
      'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
 set(gca,'fontsize',12,'fontweight','bold')
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subplot(3,3,7)
+set(gca,'xscale','log')
+hold on
 
+c4=semilogx(1./kf_Lag(1:end-2)./1e3.*2.*pi,ebs_Lag289(1:end-3).*dk_L(1:end-2)','LineWidth', ...
+    1.5,'Color',colors{6});
+
+grid on
+ylim([-3e-8,3e-8]);
+% xlim([1e-6,1e-3].*1e3)
+% xticks([1e-3,1/200,1e-2,1e-1,1/4,1])
+% xticklabels({'1/1000','1/200','1/100','1/10','1/4','1'})
+xlim([1e3,1e6]./1e3);
+xticks([1,4,1e1,1e2,200,1e3])
+xlabel('$$\mathbf{r \ [km]}$$','Interpreter','latex')
+% xlabel('$$\mathbf{k \ [1/km]}$$','Interpreter','latex')
+ylabel('$$\mathbf{\epsilon_j*dk_j  \ [m^{2}/s^{3}]}$$','Interpreter','latex')
+% legend([c1,c2,c3,c4],{['Eul CG (',win,')'],'Eul SF3-fitting (RLS)', ...
+%     ['Lag CG (',win,')'],'Lag SF3-fitting (RLS)'})
+text(0.03, 0.95, ['c) ',Casemean,' P289'], 'Units', 'normalized', ...
+     'FontSize', 12, 'FontWeight', 'bold', ...
+     'BackgroundColor', [1, 1, 0.8, 0.6], ... % 半透明背景
+     'EdgeColor', [.7,.7,.7], ... % 边框颜色
+     'Margin', 3, ... % 边距
+     'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
+set(gca,'fontsize',12,'fontweight','bold')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %625
@@ -283,11 +341,36 @@ text(0.03, 0.95, 'e) Hf P625', 'Units', 'normalized', ...
      'Margin', 3, ... % 边距
      'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
 set(gca,'fontsize',12,'fontweight','bold')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subplot(3,3,8)
+set(gca,'xscale','log')
+hold on
 
+c4=semilogx(1./kf_Lag(1:end-2)./1e3.*2.*pi,ebs_Lag625(1:end-3).*dk_L(1:end-2)','LineWidth', ...
+    1.5,'Color',colors{5});
 
+grid on
+ylim([-3e-8,3e-8]);
+% xlim([1e-6,1e-3].*1e3)
+% xticks([1e-3,1/200,1e-2,1e-1,1/4,1])
+% xticklabels({'1/1000','1/200','1/100','1/10','1/4','1'})
+xlim([1e3,1e6]./1e3);
+xticks([1,4,1e1,1e2,200,1e3])
+xlabel('$$\mathbf{r \ [km]}$$','Interpreter','latex')
+% xlabel('$$\mathbf{k \ [1/km]}$$','Interpreter','latex')
+ylabel('$$\mathbf{\epsilon_j*dk_j  \ [m^{2}/s^{3}]}$$','Interpreter','latex')
+% legend([c1,c2,c3,c4],{['Eul CG (',win,')'],'Eul SF3-fitting (RLS)', ...
+%     ['Lag CG (',win,')'],'Lag SF3-fitting (RLS)'})
+text(0.03, 0.95, ['c) ',Casemean,' P625'], 'Units', 'normalized', ...
+     'FontSize', 12, 'FontWeight', 'bold', ...
+     'BackgroundColor', [1, 1, 0.8, 0.6], ... % 半透明背景
+     'EdgeColor', [.7,.7,.7], ... % 边框颜色
+     'Margin', 3, ... % 边距
+     'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
+set(gca,'fontsize',12,'fontweight','bold')
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
+% 2500
 subplot(3,3,3)
 % 
 % patch(x_coords, y_coords, [0.9 0.9 0.9],'FaceAlpha', 0.3, ...
@@ -368,4 +451,34 @@ text(0.03, 0.95, 'f) Hf P2500', 'Units', 'normalized', ...
      'Margin', 3, ... % 边距
      'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
 set(gca,'fontsize',12,'fontweight','bold')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+subplot(3,3,9)
+set(gca,'xscale','log')
+hold on
+
+c4=semilogx(1./kf_Lag(1:end-2)./1e3.*2.*pi,ebs_Lag2500(1:end-3).*dk_L(1:end-2)','LineWidth', ...
+    1.5,'Color',colors{6});
+
+grid on
+ylim([-3e-8,3e-8]);
+% xlim([1e-6,1e-3].*1e3)
+% xticks([1e-3,1/200,1e-2,1e-1,1/4,1])
+% xticklabels({'1/1000','1/200','1/100','1/10','1/4','1'})
+xlim([1e3,1e6]./1e3);
+xticks([1,4,1e1,1e2,200,1e3])
+xlabel('$$\mathbf{r \ [km]}$$','Interpreter','latex')
+% xlabel('$$\mathbf{k \ [1/km]}$$','Interpreter','latex')
+ylabel('$$\mathbf{\epsilon_j*dk_j  \ [m^{2}/s^{3}]}$$','Interpreter','latex')
+% legend([c1,c2,c3,c4],{['Eul CG (',win,')'],'Eul SF3-fitting (RLS)', ...
+%     ['Lag CG (',win,')'],'Lag SF3-fitting (RLS)'})
+text(0.03, 0.95, ['c) ',Casemean,' P625'], 'Units', 'normalized', ...
+     'FontSize', 12, 'FontWeight', 'bold', ...
+     'BackgroundColor', [1, 1, 0.8, 0.6], ... % 半透明背景
+     'EdgeColor', [.7,.7,.7], ... % 边框颜色
+     'Margin', 3, ... % 边距
+     'VerticalAlignment', 'top', 'HorizontalAlignment', 'left')
+set(gca,'fontsize',12,'fontweight','bold')
+
+
 % saveas(gcf,['Eul_CG',win,'_vs_RLS_Fr',ini,'_',inv,'_',Case],'png')
